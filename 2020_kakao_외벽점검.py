fa5d57e106869cd599ca16f50488b14ec00ef05e
= <hease02>
2020-12-19 16:08:45 +0900

    complete

--- 2020_kakao_외벽점검.py
+++ 2020_kakao_외벽점검.py
@@ -1,21 +1,48 @@
 from collections import defaultdict
 import itertools
 
-n_list = [12, 12]
-week_list = [[1, 5, 6, 10], [1, 3, 4, 9, 10]]
-dist_list = [[1, 2, 3, 4], [3, 5, 7]]
+
+n_list = [12, 12, 1]
+week_list = [[1, 5, 6, 10], [1, 3, 4, 9, 10], [0]]
+dist_list = [[1, 2, 3, 4], [3, 5, 7], [100]]
 result_list = [2, 1]
 
 
 def solution(n, weak, dist):
-    answer = 0
+    dist_per = []
 
-    dist_dict = defaultdict(list)
+    weak_circle = weak + [n + w for w in weak]
+    del weak_circle[len(weak_circle) - 1] # 맨 마지막 원소 삭제
 
-    for n in range(len(dist)):
-        dist_dict[n] = itertools.combinations(dist, n)
+    min_value = 500
 
+    for i in range(1, len(dist)+1):
+        dist_per.extend(list(itertools.permutations(dist, i)))
 
+    for start in range(0, len(weak)):
+        for dist_tmp in dist_per:
+            dist_len = len(dist_tmp)
+
+            i = 0
+            cur = -1
+
+            check = True
+
+            for num in range(len(weak)):
+                if cur < weak_circle[start + num]:
+                    if i < dist_len:
+                        cur = weak_circle[start + num] + dist_tmp[i]
+                        i = i + 1
+                    else:
+                        check = False
+                        break
+            if check:
+                min_value = min(min_value, len(dist_tmp))
+
+    answer = min_value
+
+    if answer ==500:
+        answer = -1
 
     return answer
 
